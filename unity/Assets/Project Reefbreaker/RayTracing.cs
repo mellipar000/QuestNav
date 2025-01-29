c67abd22b9011830fcfdc9f6a24a195f3b2d1b74
mellipar000 <mellipar000@hsestudents.org>
2025-01-28 23:08:13 -0500

    Setup Nt4Client

--- unity/Assets/Project Reefbreaker/RayTracing.cs
+++ unity/Assets/Project Reefbreaker/RayTracing.cs
@@ -1,27 +1,22 @@
 using System;
+using NetworkTables;
 using UnityEngine;
 
 public class RayTracing : MonoBehaviour
 {
-    // Angles to game pieces relative to camera
-    private double xAngle, yAngle, zAngle;
+    private Nt4Client _ntClient;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // TODO: Change serverBaseAddress and potentially add serverPort, onOpen, and onNewTopicData
+        _ntClient = new Nt4Client("Reefbreaker", "127.0.0.1");
+        _ntClient.Connect();
     }
 
     // Update is called once per frame
     void Update()
     {
         
-    }
-
-    public double[] getCameraAngles() {
-        xAngle = 0.0;
-        yAngle = 0.0;
-        zAngle = 0.0;
-        return new double[] {xAngle, yAngle, zAngle};
     }
 }
